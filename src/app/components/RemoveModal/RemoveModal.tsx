ec72076132b7d879ae1d65ea17bc774239956328
Benjamin Rasoli (student syh) <benjamin.rasoli@yh.nackademin.se>
2025-10-01 16:54:09 +0200

    small fix for modal overlay issue

--- src/app/components/RemoveModal/RemoveModal.tsx
+++ src/app/components/RemoveModal/RemoveModal.tsx
@@ -3,6 +3,7 @@ import { disableOverflow } from "@/app/utils/HandleDOM";
 import { handleOutsideClick } from "@/app/utils/HandleOutsideClick";
 import { useEffect, useRef } from "react";
 import { FaWindowClose } from "react-icons/fa";
+import { createPortal } from "react-dom";
 
 function RemoveModal({
   isConfirmModalOpen,
@@ -43,51 +44,48 @@ function RemoveModal({
     setIsConfirmModalOpen(false);
   };
 
-  return (
-    <>
-      {isConfirmModalOpen && itemToRemove && (
-        <div className="fixed inset-0 flex justify-center items-center z-[55]">
-          <div
-            className="absolute inset-0 bg-black"
-            style={{ opacity: 0.5 }}
-          ></div>
-          <div
-            ref={modalRef}
-            className="bg-white dark:bg-dark w-[300px] min-h-[250px] flex flex-col rounded-lg shadow-lg p-6 relative z-[55]"
+  if (!isConfirmModalOpen || !itemToRemove) return null;
+
+  return createPortal(
+    <div className="fixed inset-0 flex justify-center items-center z-[9999]">
+      <div className="absolute inset-0 bg-black opacity-50 z-[9998]"></div>
+
+      <div
+        ref={modalRef}
+        className="bg-white dark:bg-dark w-[300px] min-h-[250px] flex flex-col rounded-lg shadow-lg p-6 relative z-[9999]"
+      >
+        <button
+          onClick={() => setIsConfirmModalOpen(false)}
+          className="absolute top-2 right-2 cursor-pointer hover:text-red-hover"
+        >
+          <FaWindowClose size={24} />
+        </button>
+
+        <p className="text-lg font-semibold text-center my-auto">
+          Are you sure you want to remove &quot;
+          <span className="font-bold text-blue break-words">
+            {itemToRemove.title || itemToRemove.name}
+          </span>
+          &quot; from your favorites?
+        </p>
+
+        <div className="flex justify-between px-5 gap-2 mt-6">
+          <button
+            onClick={handleRemoveConfirmation}
+            className="cursor-pointer bg-red hover:bg-red-hover rounded-lg p-2 text-white"
           >
-            <button
-              onClick={() => setIsConfirmModalOpen(false)}
-              className="absolute top-2 right-2 cursor-pointer hover:text-red-hover"
-            >
-              <FaWindowClose size={24} />
-            </button>
-
-            <p className="text-lg font-semibold text-center my-auto">
-              Are you sure you want to remove &quot;
-              <span className="font-bold text-blue break-words">
-                {itemToRemove.title || itemToRemove.name}
-              </span>
-              &quot; from your favorites?
-            </p>
-
-            <div className="flex justify-between px-5 gap-2">
-              <button
-                onClick={handleRemoveConfirmation}
-                className="cursor-pointer bg-red hover:bg-red-hover rounded-lg p-2 text-white"
-              >
-                Yes, Remove
-              </button>
-              <button
-                onClick={handleCancel}
-                className="cursor-pointer bg-gray-300 hover:bg-gray-400 rounded-lg p-2 text-black"
-              >
-                Cancel
-              </button>
-            </div>
-          </div>
+            Yes, Remove
+          </button>
+          <button
+            onClick={handleCancel}
+            className="cursor-pointer bg-gray-300 hover:bg-gray-400 rounded-lg p-2 text-black"
+          >
+            Cancel
+          </button>
         </div>
-      )}
-    </>
+      </div>
+    </div>,
+    document.getElementById("modal-root")!
   );
 }
 
