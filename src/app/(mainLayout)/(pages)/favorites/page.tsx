1227e982b00031343bfa8c498f74a65633f0c45c
Benjamin Rasoli (student syh) <benjamin.rasoli@yh.nackademin.se>
2025-10-02 14:05:36 +0200

    updated favorite page

--- src/app/(mainLayout)/(pages)/favorites/page.tsx
+++ src/app/(mainLayout)/(pages)/favorites/page.tsx
@@ -147,15 +147,24 @@ function Page() {
             ) : activeFilter === "tv" ? (
               <p>No TV shows found in your favorites.</p>
             ) : (
-              <p>
-                {!user
-                  ? "You must be logged in to view your favorites."
-                  : "No favorites yet. Start adding some"}
-              </p>
+              <div>
+                {!user ? (
+                  <>
+                    <p>You must be logged in to view your favorites.</p>{" "}
+                    <Link className="text-blue" href={"/login"}>
+                      Login
+                    </Link>
+                  </>
+                ) : (
+                  <>
+                    <p>No favorites yet. Start adding some</p>
+                    <Link className="text-blue" href={"/"}>
+                      Home
+                    </Link>
+                  </>
+                )}
+              </div>
             )}
-            <Link className="text-blue" href={"/"}>
-              Home
-            </Link>
           </div>
         ) : (
           <MediaCard
--- src/app/components/Loading/Loading.tsx
+++ src/app/components/Loading/Loading.tsx
@@ -3,7 +3,7 @@ import { ClipLoader } from "react-spinners";
 function Loading({ size }: { size?: number }) {
   return (
     <>
-      <ClipLoader color="#2d99ff" size={60} />
+      <ClipLoader color="#2d99ff" size={size} />
     </>
   );
 }
